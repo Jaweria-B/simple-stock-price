426666b86190ada988d470b14cd24cce061323a2
Jaweria B <bjaweria509@gmail.com>
2024-02-07 20:37:17 +0500

    updated app: included other multinational companies

--- myapp.py
+++ myapp.py
@@ -1,29 +1,43 @@
 import yfinance as yf
 import streamlit as st
 
-st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Google
-""")
-
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
 
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+st.write(
+"""
+# Simple Stock Price App
+"""
+)
+companies = [ ('GOOGL', 'Google'), ('AAPL', 'Apple'), ('AMZN', 'Amazon'), ('META', 'Meta'), ('MSFT', 'Microsoft'), ('TSLA', 'Tesla'), ('DIS', 'The Walt Disney Company')]
 
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2013-5-31', end='2023-5-31')
+for ts, cmp in companies:
 
-# tickerDf -- Simply shows the data
+    st.write(f"""
+    # {cmp}
+    Shown are the stock ***closing price*** and ***volume*** of
+    """, cmp)
+    tickerSymbol = ts 
+
+    #get data on this ticker
+    tickerData = yf.Ticker(tickerSymbol)
+
+    #get the historical prices for this ticker
+    tickerDf = tickerData.history(period='1d', start='2013-5-31', end='2024-1-31')
+
+    # tickerDf -- Simply shows the data
+
+    st.write("""
+    ## Closing Price
+    """)
+    st.line_chart(tickerDf.Close)
+
+    st.write("""
+    ## Volume Price
+    """)
+    st.line_chart(tickerDf.Volume)
+
 
 st.write("""
-## Closing Price
+Made By **_Jaweria Batool_**
 """)
-st.line_chart(tickerDf.Close)
-
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
